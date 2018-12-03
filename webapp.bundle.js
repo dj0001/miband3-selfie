ed4b50c302fc60b3b58c9a4cfcc4ad111351ac13
dj <dj0@gmx.de>
2018-12-03 14:44:51 +0100

    Update webapp.bundle.js

--- webapp.bundle.js
+++ webapp.bundle.js
@@ -7591,7 +7591,7 @@
                 const value = Buffer.from(event.target.value.buffer);
                           
                 alert(value.toString('hex'))  //dj
-                if (value.toString('hex') === '08') alert('shot')  //dj device search //alert(value.toString('hex'))  //09
+                if (value.toString('hex') == '08') alert('shot')  //dj device search //alert(value.toString('hex'))  //09
                           
                 if (event.target.uuid === this.char.auth.uuid) {
                   const cmd = value.slice(0,3).toString('hex');
