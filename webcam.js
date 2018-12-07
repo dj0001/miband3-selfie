bb8ddc5a2fae1bd4d3e7aaaf5f95a5341960685d
dj <dj0@gmx.de>
2018-12-07 05:24:29 +0000

    long tab

--- webcam.js
+++ webcam.js
@@ -1,6 +1,6 @@
 //webcam goes here
 document.querySelector('select').onchange=function(){start(this.value)}
-document.querySelector('#shot').oncontextmenu=function(){e.preventDefault();if(this.textContent=="start") {this.textContent="stop";capture()} else {this.textContent="start";clearTimeout(t)}}
+document.querySelector('#shot').oncontextmenu=function(e){e.preventDefault();if(this.textContent=="start") {this.textContent="stop";capture()} else {this.textContent="start";clearTimeout(t)}}
 document.querySelector('#shot').onclick=function(){console.log('shot');clearTimeout(t);capture()}
 
 var upload=false
